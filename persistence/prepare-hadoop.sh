ca279016bec1b32c8f8c58f6ab9887049bd532fa
Sergio Fernández <sergio.fernandez@salzburgresearch.at>
2014-07-08 16:50:30 +0200

    FRAMEWORK-43: native compilation is not required if you already are in 32 bits

--- persistence/prepare-hadoop.sh
+++ persistence/prepare-hadoop.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-VERSION=$1
-DIR="target/hadoop-$VERSION"
-
-if [ ! -d "$DIR" ] && [ ! -d "$DIR-src" ]; 
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_32' ]; 
 then
-    echo "ERROR: hadoop distributions not found at $DIR*"
-    exit -1
-fi
+    echo "No further native compilations required under 32 bits environments"
+else
+    VERSION=$1
+    DIR="target/hadoop-$VERSION"
+    if [ ! -d "$DIR" ] && [ ! -d "$DIR-src" ]; 
+    then
+        echo "ERROR: hadoop distributions not found at $DIR*"
+        exit -1
+    fi
 
-(cd $DIR-src; mvn package -Pnative -DskipTests)
-(cd $DIR/lib/native/; mkdir dist; mv *.* dist)
-cp $DIR-src/hadoop-tools/hadoop-pipes/target/native/*.a $DIR/lib/native/
-cp $DIR-src/hadoop-common-project/hadoop-common/target/native/target/usr/local/lib/libhadoop.so.1.0.0 $DIR/lib/native/libhadoop.so
-cp $DIR-src/hadoop-common-project/hadoop-common/target/native/target/usr/local/lib/libhadoop.a $DIR/lib/native/
-cp $DIR-src/hadoop-hdfs-project/hadoop-hdfs/target/native/target/usr/local/lib/libhdfs.so.0.0.0 $DIR/lib/native/libhdfs.so
-cp $DIR-src/hadoop-hdfs-project/hadoop-hdfs/target/native/target/usr/local/lib/libhdfs.a $DIR/lib/native/
+    (cd $DIR-src; mvn package -Pnative -DskipTests)
+    (cd $DIR/lib/native/; mkdir dist; mv *.* dist)
+    cp $DIR-src/hadoop-tools/hadoop-pipes/target/native/*.a $DIR/lib/native/
+    cp $DIR-src/hadoop-common-project/hadoop-common/target/native/target/usr/local/lib/libhadoop.so.1.0.0 $DIR/lib/native/libhadoop.so
+    cp $DIR-src/hadoop-common-project/hadoop-common/target/native/target/usr/local/lib/libhadoop.a $DIR/lib/native/
+    cp $DIR-src/hadoop-hdfs-project/hadoop-hdfs/target/native/target/usr/local/lib/libhdfs.so.0.0.0 $DIR/lib/native/libhdfs.so
+    cp $DIR-src/hadoop-hdfs-project/hadoop-hdfs/target/native/target/usr/local/lib/libhdfs.a $DIR/lib/native/
+fi
 
