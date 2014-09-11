04d997b96445589bd1169c0fa5b7375f7017cb20
Sebastian Schaffert <sschaffert@apache.org>
2014-09-11 11:30:17 +0200

    implement sample Java analyzer service (FRAMEWORK-67) tested integrated workflow (FRAMEWORK-69)

--- api/c++/persistence/PersistenceService.hpp
+++ api/c++/persistence/PersistenceService.hpp
@@ -14,160 +14,160 @@
 #include "rdf_query.hpp"
 
 namespace mico {
-  namespace persistence {
+    namespace persistence {
 
-    using namespace mico::rdf::query;   
+        using namespace mico::rdf::query;
 
-    class content_item_iterator;
+        class content_item_iterator;
 
-    /**
-     * Specialised support for persistence service metadata. Might in the future be extended with
-     * additional methods for simplified use of certain vocabularies.
-     */
-    class PersistenceMetadata : public Metadata {
-      friend class PersistenceService;
+        /**
+        * Specialised support for persistence service metadata. Might in the future be extended with
+        * additional methods for simplified use of certain vocabularies.
+        */
+        class PersistenceMetadata : public Metadata {
+            friend class PersistenceService;
 
-    protected:
-      PersistenceMetadata(std::string baseUrl) : Metadata(baseUrl)  {};
+        protected:
+            PersistenceMetadata(std::string baseUrl) : Metadata(baseUrl)  {};
 
-    };
-
-
-    /**
-     * Main service for accessing the MICO persistence API. The persistence service can be used for
-     * managing ContentItems stored in the peristence API of the MICO platform. Services working
-     * with ContentItems should only use instances of this class for accessing the persistence API.
-     */
-    class PersistenceService {
-
-    private:
-
-      std::string marmottaServerUrl;
-      std::string contentDirectory;
-      PersistenceMetadata metadata;
-
-    public:
-
-      /**
-       * Initialise persistence service with the address of a server running the standard installation of
-       * the MICO platform with Marmotta at port 8080 under context /marmotta, RabbitMQ at port 5672, and
-       * an FTP server, all with login/password mico/mico.
-       */ 
-      PersistenceService(std::string serverAddress) 
-	: marmottaServerUrl("http://" + serverAddress + ":8080/marmotta")
-	, metadata("http://" + serverAddress + ":8080/marmotta")
-	, contentDirectory("ftp://mico:mico@" + serverAddress) {};
+        };
 
 
-      /**
-       * Initialise persistence service with the address of a server running the standard installation of
-       * the MICO platform with Marmotta at port 8080 under context /marmotta, RabbitMQ at port 5672, and
-       * an FTP server, all with login/password mico/mico.
-       */ 
-      PersistenceService(std::string serverAddress, int marmottaPort, std::string user, std::string password) 
-	: marmottaServerUrl("http://" + serverAddress + ":" + std::to_string(marmottaPort) + "/marmotta")
-	, metadata("http://" + serverAddress + ":" + std::to_string(marmottaPort) + "/marmotta")
-	, contentDirectory("ftp://" + user + ":" + password + "@" + serverAddress) {};
+        /**
+        * Main service for accessing the MICO persistence API. The persistence service can be used for
+        * managing ContentItems stored in the peristence API of the MICO platform. Services working
+        * with ContentItems should only use instances of this class for accessing the persistence API.
+        */
+        class PersistenceService {
+
+        private:
+
+            std::string marmottaServerUrl;
+            std::string contentDirectory;
+            PersistenceMetadata metadata;
+
+        public:
+
+            /**
+            * Initialise persistence service with the address of a server running the standard installation of
+            * the MICO platform with Marmotta at port 8080 under context /marmotta, RabbitMQ at port 5672, and
+            * an FTP server, all with login/password mico/mico.
+            */
+            PersistenceService(std::string serverAddress)
+                    : marmottaServerUrl("http://" + serverAddress + ":8080/marmotta")
+                    , metadata("http://" + serverAddress + ":8080/marmotta")
+                    , contentDirectory("ftp://mico:mico@" + serverAddress) {};
 
 
-      /**
-       * Initialise an instance of the PersistenceService using the Marmotta server with the given
-       * URL as backend.
-       *
-       * @param marmottaServerUrl the URL of the Apache Marmotta server, e.g. http://localhost:8080/marmotta
-       */
-      PersistenceService(std::string marmottaServerUrl, std::string contentDirectory) 
-	: marmottaServerUrl(marmottaServerUrl), metadata(marmottaServerUrl), contentDirectory(contentDirectory) {};
+            /**
+            * Initialise persistence service with the address of a server running the standard installation of
+            * the MICO platform with Marmotta at port 8080 under context /marmotta, RabbitMQ at port 5672, and
+            * an FTP server, all with login/password mico/mico.
+            */
+            PersistenceService(std::string serverAddress, int marmottaPort, std::string user, std::string password)
+                    : marmottaServerUrl("http://" + serverAddress + ":" + std::to_string(marmottaPort) + "/marmotta")
+                    , metadata("http://" + serverAddress + ":" + std::to_string(marmottaPort) + "/marmotta")
+                    , contentDirectory("ftp://" + user + ":" + password + "@" + serverAddress) {};
 
 
-      /**
-       * Get a handle on the overall metadata storage of the persistence service. This can e.g. be used for querying
-       * about existing content items.
-       *
-       * @return
-       */
-      PersistenceMetadata& getMetadata() { return metadata; };
-
-      /**
-       * Create a new content item with a random URI and return it. The content item should be suitable for reading and
-       * updating and write all updates to the underlying low-level persistence layer.
-       *
-       * @return a handle to the newly created ContentItem
-       */
-      ContentItem* createContentItem();
-
-      /**
-       * Create a new content item with the given URI and return it. The content item should be suitable for reading and
-       * updating and write all updates to the underlying low-level persistence layer.
-       *
-       * @return a handle to the newly created ContentItem
-       */
-      ContentItem* createContentItem(const mico::rdf::model::URI& id);
+            /**
+            * Initialise an instance of the PersistenceService using the Marmotta server with the given
+            * URL as backend.
+            *
+            * @param marmottaServerUrl the URL of the Apache Marmotta server, e.g. http://localhost:8080/marmotta
+            */
+            PersistenceService(std::string marmottaServerUrl, std::string contentDirectory)
+                    : marmottaServerUrl(marmottaServerUrl), metadata(marmottaServerUrl), contentDirectory(contentDirectory) {};
 
 
-      /**
-       * Return the content item with the given URI if it exists. The content item should be suitable for reading and
-       * updating and write all updates to the underlying low-level persistence layer.
-       *
-       * @return a handle to the ContentItem with the given URI, or null if it does not exist
-       */
-      ContentItem* getContentItem(const mico::rdf::model::URI& id);
+            /**
+            * Get a handle on the overall metadata storage of the persistence service. This can e.g. be used for querying
+            * about existing content items.
+            *
+            * @return
+            */
+            PersistenceMetadata& getMetadata() { return metadata; };
 
-      /**
-       * Delete the content item with the given URI. If the content item does not exist, do nothing.
-       */
-      void deleteContentItem(const mico::rdf::model::URI& id);
+            /**
+            * Create a new content item with a random URI and return it. The content item should be suitable for reading and
+            * updating and write all updates to the underlying low-level persistence layer.
+            *
+            * @return a handle to the newly created ContentItem
+            */
+            ContentItem* createContentItem();
 
-      /**
-       * Return an iterator over all currently available content items.
-       *
-       * @return iterable
-       */
-      content_item_iterator begin();
-
-
-      /**
-       * Return the end iterator for checking when iteration has completed.
-       */
-      content_item_iterator end();
+            /**
+            * Create a new content item with the given URI and return it. The content item should be suitable for reading and
+            * updating and write all updates to the underlying low-level persistence layer.
+            *
+            * @return a handle to the newly created ContentItem
+            */
+            ContentItem* createContentItem(const mico::rdf::model::URI& id);
 
 
+            /**
+            * Return the content item with the given URI if it exists. The content item should be suitable for reading and
+            * updating and write all updates to the underlying low-level persistence layer.
+            *
+            * @return a handle to the ContentItem with the given URI, or null if it does not exist
+            */
+            ContentItem* getContentItem(const mico::rdf::model::URI& id);
 
-    };
+            /**
+            * Delete the content item with the given URI. If the content item does not exist, do nothing.
+            */
+            void deleteContentItem(const mico::rdf::model::URI& id);
+
+            /**
+            * Return an iterator over all currently available content items.
+            *
+            * @return iterable
+            */
+            content_item_iterator begin();
+
+
+            /**
+            * Return the end iterator for checking when iteration has completed.
+            */
+            content_item_iterator end();
+
+
+
+        };
 
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
-    /**
-     * 	Internal implementation of iterators over the content items managed by a PersistenceService
-     */
-    class content_item_iterator  : public boost::iterator_facade<content_item_iterator, ContentItem*, boost::forward_traversal_tag, ContentItem*> {
-    private:
-      int pos;
-      const std::string& baseUrl;
-      const std::string& contentDirectory;
-      const mico::rdf::query::TupleResult* result;
+        /**
+        * 	Internal implementation of iterators over the content items managed by a PersistenceService
+        */
+        class content_item_iterator  : public boost::iterator_facade<content_item_iterator, ContentItem*, boost::forward_traversal_tag, ContentItem*> {
+        private:
+            int pos;
+            const std::string& baseUrl;
+            const std::string& contentDirectory;
+            const mico::rdf::query::TupleResult* result;
 
-    public:
-      content_item_iterator(const std::string& baseUrl, const std::string& contentDirectory) 
-	: baseUrl(baseUrl), contentDirectory(contentDirectory), pos(-1), result(NULL) {};
+        public:
+            content_item_iterator(const std::string& baseUrl, const std::string& contentDirectory)
+                    : baseUrl(baseUrl), contentDirectory(contentDirectory), pos(-1), result(NULL) {};
 
-      content_item_iterator(const std::string& baseUrl, const std::string& contentDirectory, const mico::rdf::query::TupleResult* r) 
-	: baseUrl(baseUrl), contentDirectory(contentDirectory), pos(0), result(r) {};
+            content_item_iterator(const std::string& baseUrl, const std::string& contentDirectory, const mico::rdf::query::TupleResult* r)
+                    : baseUrl(baseUrl), contentDirectory(contentDirectory), pos(0), result(r) {};
 
-      ~content_item_iterator() { if(result) { delete result; } };
+            ~content_item_iterator() { if(result) { delete result; } };
 
-      
-    private:
 
-      friend class boost::iterator_core_access;
+        private:
 
-      void increment();
-      bool equal(content_item_iterator const& other) const;
-      ContentItem* dereference() const;
+            friend class boost::iterator_core_access;
 
-    };
+            void increment();
+            bool equal(content_item_iterator const& other) const;
+            ContentItem* dereference() const;
+
+        };
 #endif
-  }
+    }
 }
 
 #endif
---
+++ api/java/samples/src/main/java/eu/mico/platform/samples/wordcount/WordCountAnalyzer.java
@@ -0,0 +1,150 @@
+package eu.mico.platform.samples.wordcount;
+
+import eu.mico.platform.event.api.AnalysisResponse;
+import eu.mico.platform.event.api.AnalysisService;
+import eu.mico.platform.event.api.EventManager;
+import eu.mico.platform.event.impl.EventManagerImpl;
+import eu.mico.platform.event.model.AnalysisException;
+import eu.mico.platform.persistence.model.Content;
+import eu.mico.platform.persistence.model.ContentItem;
+import org.apache.commons.io.IOUtils;
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.LiteralImpl;
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.model.vocabulary.DCTERMS;
+import org.openrdf.repository.RepositoryException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This is a sample analyzer demonstrating how to use the MICO Java Client API to implement analyzers. It takes
+ * a plain text as input and generates a word count as output, using an RDF property to attach to a new content part.
+ *
+ * @author Sebastian Schaffert (sschaffert@apache.org)
+ */
+public class WordCountAnalyzer implements AnalysisService {
+
+    private static Logger log = LoggerFactory.getLogger(WordCountAnalyzer.class);
+
+    private static SimpleDateFormat isodate = new SimpleDateFormat("yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\'", DateFormatSymbols.getInstance(Locale.US));
+    static {
+        isodate.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+
+    @Override
+    public URI getServiceID() {
+        return new URIImpl("http://www.mico-project.org/services/wordcount");
+    }
+
+    @Override
+    public String getProvides() {
+        return "mico/wordcount";
+    }
+
+    @Override
+    public String getRequires() {
+        return "text/plain";
+    }
+
+    @Override
+    public String getQueueName() {
+        return "wordcount";
+    }
+
+    @Override
+    public void call(AnalysisResponse analysisResponse, ContentItem contentItem, URI uri) throws AnalysisException, IOException {
+        try {
+            // get the content part with the given URI
+            Content content = contentItem.getContentPart(uri);
+
+            // get the input stream and read it into a string
+            String text = IOUtils.toString(content.getInputStream(), "utf-8");
+
+            // count the words using a regular expression pattern
+            Pattern p_wordcount = Pattern.compile("\\w+");
+            Matcher m = p_wordcount.matcher(text);
+
+            int count;
+            for(count = 0; m.find(); count++);
+
+
+            // create a new content part for assigning the metadata
+            Content result = contentItem.createContentPart();
+            result.setType(getProvides());
+            result.setRelation(DCTERMS.CREATOR, getServiceID());  // set the service ID as provenance information for the new content part
+            result.setRelation(DCTERMS.SOURCE, uri);              // set the analyzed content part as source for the new content part
+            result.setProperty(DCTERMS.CREATED, isodate.format(new Date())); // set the created date for the new content part
+
+            // add the wordcount as property
+            result.setProperty(new URIImpl("http://www.mico-project.org/properties/wordcount"), Integer.toString(count));
+
+            // report newly available results to broker
+            analysisResponse.sendMessage(contentItem, result.getURI());
+
+        } catch (RepositoryException e) {
+            log.error("error accessing metadata repository",e);
+
+            throw new AnalysisException("error accessing metadata repository",e);
+        }
+    }
+
+
+    // usage: java WordCountAnalyzer <hostname> <user> <password>
+    public static void main(String[] args) {
+        if(args.length != 3) {
+            System.err.println("Usage: java WordCountAnalyzer <hostname> <user> <password>");
+            System.exit(1);
+        }
+
+        String mico_host = args[0];
+        String mico_user = args[1];
+        String mico_pass = args[2];
+
+        try {
+            // create event manager instance, providing the correct host, user and password, and initialise it
+            EventManager eventManager = new EventManagerImpl(mico_host,mico_user,mico_pass);
+            eventManager.init();
+
+            // create analyzer service instance and register it with event manager
+            WordCountAnalyzer svc_wc = new WordCountAnalyzer();
+            eventManager.registerService(svc_wc);
+
+
+            // keep running service in the background, and wait for user command "q" on the frontent to terminate
+            // service (other approaches might be more sensible for a service, e.g. commons-daemon)
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+            char c = ' ';
+            while(Character.toLowerCase(c) != 'q') {
+                System.out.print("enter 'q' to quit: ");
+                System.out.flush();
+
+                c = in.readLine().charAt(0);
+            }
+
+            // unregister service before quiting
+            eventManager.unregisterService(svc_wc);
+
+            // shutdown event manager properly
+            eventManager.shutdown();
+
+            System.out.println("WordCountAnalyzer shutdown completed");
+            // DONE
+        } catch (IOException e) {
+            log.error("error while accessing event manager:",e);
+        }
+
+    }
+}
