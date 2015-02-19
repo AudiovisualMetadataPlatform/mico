7e52f0ed0c61fc472e875be0e3db9c9f8c4754a8
Horst Stadler <horst.stadler@salzburgresearch.at>
2015-02-19 15:49:26 +0100

    FRAMEWORK-124: Added dependencies and basic HDFS access.

--- storage/src/main/java/eu/mico/platform/storage/impl/StorageServiceHDFS.java
+++ storage/src/main/java/eu/mico/platform/storage/impl/StorageServiceHDFS.java
@@ -4,36 +4,71 @@ import eu.mico.platform.storage.api.StorageService;
 import eu.mico.platform.storage.model.Content;
 import eu.mico.platform.storage.model.ContentItem;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Collection;
+import java.util.HashMap;
+
+import org.apache.commons.io.input.ProxyInputStream;
+import org.apache.commons.io.output.ProxyOutputStream;
+import org.apache.commons.lang.NotImplementedException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 /**
  * HDFS-based storage implementation
+ *
+ * @author Horst Stadler
  */
 public class StorageServiceHDFS implements StorageService {
 
-    private final String host, user, pass;
+    private final Configuration hdfsConfig;
 
-    public StorageServiceHDFS(String host, String user, String pass) {
-        this.host = host;
-        this.user = user;
-        this.pass = pass;
+    public StorageServiceHDFS(String host) throws IOException {
+        hdfsConfig = new Configuration();
+        hdfsConfig.set("fs.defaultFS", "hdfs://" + host);
+    }
+
+    public StorageServiceHDFS(HashMap<String, String> params) throws IOException {
+        this(params.get("host") != null ? params.get("host") : "localhost");
     }
 
     @Override
     public Collection<ContentItem> list() {
-        return null; //TODO
+        throw new NotImplementedException(); //TODO
     }
 
     @Override
-    public OutputStream getOutputStream(Content part) {
-        return null;
+    public OutputStream getOutputStream(Content part) throws IOException {
+        final FileSystem fs = FileSystem.get(this.hdfsConfig);
+        Path path = getContentPartPath(part);
+        return new ProxyOutputStream(fs.create(path)) {
+            @Override
+            public void close() throws IOException {
+                super.close();
+                fs.close();
+            }
+        };
+
     }
 
     @Override
-    public InputStream getInputStream(Content part) {
-        return null;
+    public InputStream getInputStream(Content part) throws IOException {
+        final FileSystem fs = FileSystem.get(this.hdfsConfig);
+        Path path = getContentPartPath(part);
+        return new ProxyInputStream(fs.open(path)) {
+            @Override
+            public void close() throws IOException {
+                super.close();
+                fs.close();
+            }
+        };
+    }
+
+    private Path getContentPartPath(Content part) {
+        return new Path(null, null, "/" + part.getId() + ".bin");
     }
 
 }
