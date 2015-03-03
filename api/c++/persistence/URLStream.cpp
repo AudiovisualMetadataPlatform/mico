995d80de25951b580ae5ea3fe2d460cc1e987bd3
Horst Stadler <horst.stadler@salzburgresearch.at>
2015-03-03 17:48:19 +0100

    FRAMEWORK-125: Restructured URL handler to support FileStream, WebStream and HDFSStream.

---
+++ api/c++/persistence/URLStream.cpp
@@ -0,0 +1,147 @@
+/**
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include "FileStream.hpp"
+#include "HDFSStream.hpp"
+#include "WebStream.hpp"
+
+#include "URLStream.hpp"
+
+
+namespace mico {
+    namespace io {
+
+
+        url_ostream::url_ostream(const char* url) : std::ostream(getStreamHandler(url)) {};
+
+        url_ostream::~url_ostream() {
+            if (rdbuf() != NULL) {
+                rdbuf()->pubsync();
+                delete rdbuf();
+            }
+        }
+
+        std::streambuf* url_ostream::getStreamHandler(const char* url) {
+            url_components url_parsed = getURLComponents(url);
+            URLType url_type = getType(url_parsed);
+
+            switch (url_type) {
+                case URL_TYPE_FILE:
+                    return new FileOStream(url_parsed.path.c_str());
+                case URL_TYPE_HTTP:
+                case URL_TYPE_FTP:
+                    return new WebOStreambuf(url);
+                case URL_TYPE_HDFS:
+                    return new HDFSOStream(url_parsed.path.c_str(), url_parsed.host.c_str(), url_parsed.port);
+            }
+            throw std::string("Invalid URL: ") + url;
+        }
+
+        url_istream::url_istream(const char *url) : std::istream(getStreamHandler(url)) {}
+
+        url_istream::~url_istream() {
+            if (rdbuf() != NULL) {
+                delete rdbuf();
+            }
+        }
+
+        std::streambuf* url_istream::getStreamHandler(const char *url) {
+            url_components url_parsed = getURLComponents(url);
+            URLType url_type = getType(url_parsed);
+
+            switch (url_type) {
+                case URL_TYPE_FILE:
+                    return new FileIStream(url_parsed.path.c_str());
+                case URL_TYPE_HTTP:
+                case URL_TYPE_FTP:
+                    return new WebIStreambuf(url);
+                case URL_TYPE_HDFS:
+                    return new HDFSIStream(url_parsed.path.c_str(), url_parsed.host.c_str(), url_parsed.port);
+            }
+            throw std::string("Invalid URL: ") + url;
+        }
+
+
+        namespace {
+            URLType getType(url_components url) {
+                std::string scheme = url.scheme;
+                std::transform(scheme.begin(), scheme.end(), scheme.begin(), ::tolower);
+                if (scheme == "http" || scheme == "https") {
+                    return URL_TYPE_HTTP;
+                } else if (scheme == "ftp") {
+                    return URL_TYPE_FTP;
+                } else if (scheme == "file" || (scheme == "" && url.path != "")) {
+                    return URL_TYPE_FILE;
+                } else if (scheme == "hdfs") {
+                    return URL_TYPE_HDFS;
+                }
+                return URL_TYPE_UNKNOWN;
+            }
+
+            url_components getURLComponents(const char* url) {
+                std::string url_s = std::string(url);
+                /*
+                * Examples the regexp can handle:
+                *  /path/to/file
+                *  file:///path/to/file
+                *  ftp://127.0.0.1/
+                *  ftp://localhost/path/to/file
+                *  ftp://domain.com/path/to/file
+                *  hdfs://localhost:9000/path/to/file
+                *  hdfs://user:pwd@localhost:9000/path/to/file
+                *  hdfs://user:pwd@localhost/path/to/file
+                *  hdfs://user@localhost/path/to/file
+                */
+                /*
+                * Regex groups:
+                * 1:   scheme: ([[:alpha:]][[:alnum:]\+\.-]*?)://
+                * 2,3: username and password: (?:([[:alnum:]]+?)(?::([[:alnum:]]+?))?@)?
+                * 4:   domain/ip: ([\.[:alnum:]]+?)
+                * 5:   port: (?::([[:digit:]]+?))?
+                * 6:   path (including query and fragments: (/.*)
+                */
+                std::regex uri_pattern("^(?:([[:alpha:]][[:alnum:]\\+\\.-]*?)://(?:(?:([[:alnum:]]+?)(?::([[:alnum:]]+?))?@)?([\\.[:alnum:]]+?)(?::([[:digit:]]+?))?)?)?(/.*)");
+                std::smatch result;
+                std::regex_search(url_s, result, uri_pattern);
+
+                url_components retval;
+                retval.scheme = result[1].str();
+                retval.username = result[2].str();
+                retval.password = result[3].str();
+                retval.host = result[4].str();
+                retval.port = std::strtol(result[5].str().c_str(), NULL, 0);
+                retval.path = result[6].str();
+
+                /*for (int i = 0; i < result.length(); i++) {
+                    std::cout << "[" << i << "]:" << result[i].str() << "|" << std::endl;
+                }
+                std::cout << "Prefix:" << result.prefix().str() << "|" << std::endl;
+                std::cout << "Suffix:" << result.suffix().str() << "|" << std::endl;
+                */
+
+                return retval;
+            }
+
+
+
+
+        }
+
+
+    }
+}
---
+++ api/c++/persistence/URLStream.hpp
@@ -0,0 +1,86 @@
+#ifndef HAVE_URL_STREAM_H
+#define HAVE_URL_STREAM_H 1
+
+#include <istream>
+#include <ostream>
+#include <streambuf>
+
+namespace mico {
+    namespace io {
+
+        /*
+        * Internal data types and functions.
+        */
+        namespace {
+            enum URLType {
+                URL_TYPE_UNKNOWN = 0,
+                URL_TYPE_FILE =    1,
+                URL_TYPE_HTTP =    2,
+                URL_TYPE_FTP  =    3,
+                URL_TYPE_HDFS =    4
+            };
+
+            /*
+            * Parsed URL components
+            *
+            * @param port The port number. 0 means not specified.
+            */
+            struct url_components {
+                std::string scheme;
+                std::string host;
+                uint16_t port;
+                std::string path;
+                std::string username;
+                std::string password;
+            };
+
+            /*
+            * Takes an URL or absolute path (with UNIX style delimiters) to parse it. It does no validation of the
+            * URL/path, therefore the result of malformed (or maybe even unusual) input can lead to incorrect results.
+            *
+            * @param url The URL string to parse
+            * @return The identified URL components. If everything is empty (and port == 0) the URL could not be parsed.
+            */
+            static url_components getURLComponents(const char* url);
+
+            /*
+            * Returns the URL type.
+            *
+            * @param url The parsed URL.
+            * @return URL type.
+            */
+            static URLType getType(url_components url);
+        }
+
+        /**
+        * Main type for opening an output stream to an URL for writing. Use url_ostream(URL) to open a
+        * new stream, and normal stream operators for sending data (i.e.  \<\<).
+        */
+        class url_ostream : public std::ostream {
+        public:
+            url_ostream(const char* url);
+            url_ostream(std::string url) : url_ostream(url.c_str()) {};
+            ~url_ostream();
+
+        private:
+            static std::streambuf* getStreamHandler(const char* url);
+        };
+
+
+        /**
+        * Main type for opening an input stream to an URL for reading. Use url_istream(URL) to open a
+        * new stream, and normal stream operators for receiving data (i.e. \>\>).
+        */
+        class url_istream : public std::istream {
+        public:
+            url_istream(const char* url);
+            url_istream(std::string url) : url_istream(url.c_str()) {};
+            ~url_istream();
+
+        private:
+            static std::streambuf* getStreamHandler(const char* url);
+        };
+    }
+}
+
+#endif
