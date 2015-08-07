531e2eb836b395e16daa6230bee04318f9b9d8b0
Marcel Sieland <sld@idmt.fraunhofer.de>
2015-08-07 09:02:45 +0200

    make serviceURI configurable, add part uri from analyze response to msg header

--- rabbitmq-comp/src/main/java/eu/mico/platform/camel/MicoRabbitProducer.java
+++ rabbitmq-comp/src/main/java/eu/mico/platform/camel/MicoRabbitProducer.java
@@ -23,59 +23,73 @@ import eu.mico.platform.event.model.Event.AnalysisEvent;
  * and sends them to the extractors via RabbitMQ
  */
 public class MicoRabbitProducer extends DefaultProducer {
+    public static final String KEY_MICO_ITEM = "mico_item";
+    public static final String KEY_MICO_PART = "mico_part";
+
     private static final Logger LOG = LoggerFactory.getLogger(MicoRabbitProducer.class);
     private MicoRabbitEndpoint endpoint;
+    private String serviceId;
 
     public MicoRabbitProducer(MicoRabbitEndpoint endpoint) {
         super(endpoint);
         this.endpoint = endpoint;
+        this.serviceId = endpoint.getServiceId();
     }
 
     @Override
     public void process(Exchange exchange) throws Exception {
-    	LOG.info("{} - P R O D U C E analyze event and put it to msg body", endpoint.getEndpointKey());
-    	String content = exchange.getIn().getBody(String.class);
+        LOG.info("{} - P R O D U C E analyze event and put it to msg body", endpoint.getEndpointKey());
         AnalysisEvent event;
-    	try{
-    	    String[] lines = content.split("\r\n");
-    	    String item = lines[0];
-    	    String part = lines[1];
-            LOG.info("try to generate event for {} {}",item,part);
-    	    event = generateEvent(item,part);
-    	}catch(Exception e){
-    	    log.warn("unable to extract content item and part from message",e);
-            event = generateEvent();
-    	}
-    	exchange.getIn().setBody(event);
-        // create a new channel for the content item so it runs isolated from the other items
+        try {
+            String item = exchange.getIn().getHeader(KEY_MICO_ITEM, String.class);
+            String part = exchange.getIn().getHeader(KEY_MICO_PART, String.class);
+            if (item != null && part != null) {
+                log.info("found content part in msg HEADER: {}" , part);
+            }else{
+                // try to read item and part uri from msg body
+                String content = exchange.getIn().getBody(String.class);
+                String[] lines = content.split("\r\n");
+                item = lines[0];
+                part = lines[1];
+                exchange.getIn().setHeader(KEY_MICO_ITEM, item);
+                exchange.getIn().setHeader(KEY_MICO_PART, part);
+            }
+            event = generateEvent(item, part);
+        } catch (Exception e) {
+            log.warn("unable to extract content item and part uri from message", e);
+            event = generateEventSample();
+        }
+        exchange.getIn().setBody(event);
+        // create a new channel for the content item
+        // so it runs isolated from the other items
         Channel channel = endpoint.getConnection().createChannel();
 
-    	
         AnalyseManager manager = new AnalyseManager(event, channel);
-		manager.sendEvent();
-        if(exchange.getPattern().equals(ExchangePattern.InOut)){
-        	while(!manager.hasResponse()){
-	        	LOG.info("..waiting for response..");
-	        	Thread.sleep(300);
-        	}
+        manager.sendEvent();
+        if(exchange.getPattern().equals(ExchangePattern.InOut)||true){
+            while (!manager.hasResponse()) {
+                LOG.info("..waiting for response..");
+                Thread.sleep(300);
+            }
+            // save new part in header to tell next extractor to process that part
+            exchange.getIn().setHeader(KEY_MICO_PART, manager.getNewObjectUri());
         }
-        LOG.info("extraction finished");
+        LOG.info("{} - extraction finished");
     }
-    
-    
-    public AnalysisEvent generateEvent() {
-		LOG.info("generate PREDEFINED event");
+
+    public AnalysisEvent generateEventSample() {
+        LOG.info("generate PREDEFINED event");
         String item = "http://mico-platform:8080/marmotta/c04b7656-684d-44af-a3b7-a816ae45e7df";
         String part = "http://mico-platform:8080/marmotta/c04b7656-684d-44af-a3b7-a816ae45e7df/e61153e7-8618-472d-9c0b-b4a22d8249f4";
         return generateEvent(item, part);
     }
 
     private AnalysisEvent generateEvent(String item, String part) {
-        String service = endpoint.getServiceId();
+        LOG.info("generate event for {} {}", serviceId, part);
         Event.AnalysisEvent analysisEvent = Event.AnalysisEvent.newBuilder()
                 .setContentItemUri(item)
                 .setObjectUri(part)
-                .setServiceId(service).build();
+                .setServiceId(serviceId).build();
     	
     	return analysisEvent;
     }
@@ -89,6 +103,7 @@ public class MicoRabbitProducer extends DefaultProducer {
         private String           queue;
         private AnalysisEvent    event;
         private boolean response = false;
+        private String newObjectUri = null;
 
         public AnalyseManager(AnalysisEvent event, Channel channel) throws IOException {
             super(channel);
@@ -96,80 +111,78 @@ public class MicoRabbitProducer extends DefaultProducer {
 
             // create a reply-to queue for this content item and attach a transition consumer to it
             queue = getChannel().queueDeclare().getQueue();
-            log.info("listen on queue {} for result. ",queue);
+            log.debug("listen on queue {} for result. ",queue);
             getChannel().basicConsume(queue, false, this);
             getChannel().confirmSelect();
         }
 
+        public void sendEvent() throws IOException {
+            String correlationId = UUID.randomUUID().toString();
 
-		public void sendEvent() throws IOException {
-			String correlationId = UUID.randomUUID().toString();
+            AMQP.BasicProperties ciProps = new AMQP.BasicProperties.Builder()
+                    .correlationId(correlationId).replyTo(queue).build();
 
-			AMQP.BasicProperties ciProps = new AMQP.BasicProperties.Builder()
-					.correlationId(correlationId).replyTo(queue).build();
+            getChannel().basicPublish("", event.getServiceId(), ciProps,
+                    event.toByteArray());
 
-			getChannel().basicPublish("", event.getServiceId(), ciProps,
-					event.toByteArray());
-
-		}
+        }
 
         /**
          * Handle response of a service with an analysis event in the replyto queue
          */
-		@Override
-		public void handleDelivery(String consumerTag, Envelope envelope,
-				AMQP.BasicProperties properties, byte[] body)
-				throws IOException {
-			Event.AnalysisEvent analysisResponse = Event.AnalysisEvent
-					.parseFrom(body);
-			log.info(
-					"received processing result from service {} for content item {}: new object {}",
-					analysisResponse.getServiceId(),
-					analysisResponse.getContentItemUri(),
-					analysisResponse.getObjectUri());
-			response=true;
-			getChannel().basicAck(envelope.getDeliveryTag(), false);
-		}
+        @Override
+        public void handleDelivery(String consumerTag, Envelope envelope,
+                AMQP.BasicProperties properties, byte[] body)
+                throws IOException {
+            Event.AnalysisEvent analysisResponse = Event.AnalysisEvent
+                    .parseFrom(body);
+            newObjectUri = analysisResponse.getObjectUri();
+            log.debug(
+                    "received processing result from service {} for content item {}: new object {}",
+                    analysisResponse.getServiceId(),
+                    analysisResponse.getContentItemUri(), newObjectUri);
+            response = true;
+            getChannel().basicAck(envelope.getDeliveryTag(), false);
+        }
 
-		@Override
-		public void handleCancel(String consumerTag) throws IOException {
-			log.warn("Cancel: {}",consumerTag);
-			super.handleCancel(consumerTag);
-		}
+        @Override
+        public void handleCancel(String consumerTag) throws IOException {
+            log.warn("Cancel: {}", consumerTag);
+            super.handleCancel(consumerTag);
+        }
 
-		@Override
-		public void handleCancelOk(String consumerTag){ 
-			log.warn("CancelOk: {}",consumerTag);
-			super.handleCancelOk(consumerTag);
-		}
-		
-		@Override
-		public void handleConsumeOk(String consumerTag) {
-			log.warn("ConsumeOk: {}",consumerTag);
-			super.handleConsumeOk(consumerTag);
-		}
-		
-		@Override
-		public void handleRecoverOk(String consumerTag) {
-			log.warn("RecoverOk: {}",consumerTag);
-			super.handleRecoverOk(consumerTag);
-		}
-		
-		@Override
-		public void handleShutdownSignal(String consumerTag,
-				ShutdownSignalException sig) {
-			log.warn("ShutdownSignal: {} for consumer {}",sig,consumerTag);
-			super.handleShutdownSignal(consumerTag, sig);
-		}
+        @Override
+        public void handleCancelOk(String consumerTag) {
+            log.warn("CancelOk: {}", consumerTag);
+            super.handleCancelOk(consumerTag);
+        }
 
+        @Override
+        public void handleConsumeOk(String consumerTag) {
+            log.warn("ConsumeOk: {}", consumerTag);
+            super.handleConsumeOk(consumerTag);
+        }
 
-		public boolean hasResponse() {
-			return response;
-		}
+        @Override
+        public void handleRecoverOk(String consumerTag) {
+            log.warn("RecoverOk: {}", consumerTag);
+            super.handleRecoverOk(consumerTag);
+        }
 
+        @Override
+        public void handleShutdownSignal(String consumerTag,
+                ShutdownSignalException sig) {
+            log.warn("ShutdownSignal: {} for consumer {}", sig, consumerTag);
+            super.handleShutdownSignal(consumerTag, sig);
+        }
 
-		public void setResponse(boolean hasResponse) {
-			this.response = hasResponse;
-		}
-	}
+        public boolean hasResponse() {
+            return response;
+        }
+
+        public String getNewObjectUri() {
+            return newObjectUri;
+        }
+
+    }
 }
--- rabbitmq-comp/src/test/java/eu/mico/platform/camel/MicoRabbitComponentTest.java
+++ rabbitmq-comp/src/test/java/eu/mico/platform/camel/MicoRabbitComponentTest.java
@@ -1,14 +1,31 @@
 package eu.mico.platform.camel;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.commons.io.IOUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import de.fraunhofer.idmt.camel.MicoCamel;
 
+/**
+ * @author sld
+ *
+ */
 public class MicoRabbitComponentTest extends CamelTestSupport {
+
+    private static final String TEST_DATA = "src/test/resources/data";
+    private static MicoCamel micoCamel;
+
 
     @Test
     public void testMicoRabbit() throws Exception {
@@ -21,8 +38,6 @@ public class MicoRabbitComponentTest extends CamelTestSupport {
     @Test
     @Ignore
     public void testMicoCamel() throws Exception {
-    	MicoCamel micoCamel = new MicoCamel();
-		micoCamel.init();
 		micoCamel.testSimpleAnalyse();
     }
 
@@ -32,16 +47,55 @@ public class MicoRabbitComponentTest extends CamelTestSupport {
             public void configure() {
             	getContext().setDelayer(300L);                
                 
-                from("mico-comp://foo").pipeline()
-//                  .inOut("mico-comp://bar")
-                  .to("mico-comp://baz")
-                  .to("mock:result");
+//                from("mico-comp://foo").pipeline()
+////                  .inOut("mico-comp://bar")
+//                  .to("mico-comp://baz")
+//                  .to("mock:result");
                 
-                from("file:src/test/resources/data?startingDirectoryMustExist=true&startingDirectoryMustExist=true&initialDelay=100&delay=5000")
+                from("file:"
+                        + TEST_DATA
+                        + "?startingDirectoryMustExist=true&startingDirectoryMustExist=true&initialDelay=100&delay=5000").pipeline()
                 .to("mico-comp:vbox?host=mico-platform&user=mico&password=mico&serviceId=wordcount")
+                .to("mico-comp://foo?serviceId=foo")
+                .to("mico-comp://bar?serviceId=bar")
                 .to("mock:result");
             }
         };
+    }
+
+    @BeforeClass
+    static public void init() throws Exception {
+        resetDataFolder();
+
+        micoCamel = new MicoCamel();
+        micoCamel.init();
+    }
+    
+    @AfterClass
+    static public void cleanup(){
+        resetDataFolder();
+    }
+
+    /**
+     * camel stores all processed files in .camel folder, 
+     * move them back and delete the folder
+     */
+    private static void resetDataFolder() {
+        Path destPath = new File(TEST_DATA).toPath();
+        File processed = new File(TEST_DATA,".camel");
+        if(processed.exists()){
+            for (File f : processed.listFiles()){
+                Path source = f.toPath();
+                try {
+                    System.out.println("move file back to scr folder: " + source.getFileName());
+                    Files.move(source, destPath.resolve(source.getFileName()));
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+         processed.delete();
+        }
     }
     
     
