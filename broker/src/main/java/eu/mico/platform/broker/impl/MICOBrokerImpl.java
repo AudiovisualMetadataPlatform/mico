6b8985bbf6ef76e494b2a1adde66ecc5e4c2a7e0
Horst Stadler <horst.stadler@salzburgresearch.at>
2016-01-29 14:48:13 +0100

    FRAMEWORK-221: Fixed (the most urgent) resource leak.

--- broker/src/main/java/eu/mico/platform/broker/impl/MICOBrokerImpl.java
+++ broker/src/main/java/eu/mico/platform/broker/impl/MICOBrokerImpl.java
@@ -403,6 +403,7 @@ public class MICOBrokerImpl implements MICOBroker {
         private ContentItem      item;
         private ContentItemState state;
         private String           queue;
+        private String           queueTag;
 
         public ContentItemManager(ContentItem item, ContentItemState state, Channel channel) throws IOException {
             super(channel);
@@ -411,7 +412,7 @@ public class MICOBrokerImpl implements MICOBroker {
 
             // create a reply-to queue for this content item and attach a transition consumer to it
             queue = getChannel().queueDeclare().getQueue();
-            getChannel().basicConsume(queue, false, this);
+            queueTag = getChannel().basicConsume(queue, false, this);
         }
 
 
@@ -486,6 +487,13 @@ public class MICOBrokerImpl implements MICOBroker {
                 log.debug("Exception:",ex);
             } catch (InterruptedException e) {
                 log.error("analysis of content item {} was interrupted ...", item.getURI().stringValue());
+            } finally {
+                try {
+                    getChannel().basicCancel(this.queueTag);
+                    getChannel().close();
+                } catch (IOException e) {
+                    log.error("unable to unregister queue and closing channel for content item {}", item.getURI().stringValue());
+                }
             }
         }
     }
