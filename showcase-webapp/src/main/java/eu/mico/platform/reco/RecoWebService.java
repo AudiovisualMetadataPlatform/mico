37d5d0f21db33672ee19f6dc51a9f7a8bfd029b3
Thomas Köllmer <thomas.koellmer@idmt.fraunhofer.de>
2016-02-16 18:37:54 +0100

    started platform integration

--- showcase-webapp/src/main/java/eu/mico/platform/reco/RecoWebService.java
+++ showcase-webapp/src/main/java/eu/mico/platform/reco/RecoWebService.java
@@ -4,6 +4,7 @@ package eu.mico.platform.reco;
 import eu.mico.platform.broker.api.MICOBroker;
 import eu.mico.platform.event.api.EventManager;
 import eu.mico.platform.persistence.api.PersistenceService;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Path("/reco")
 public class RecoWebService {
@@ -36,6 +39,28 @@ public class RecoWebService {
     @Produces("text/plain")
     public Response getTestResponse() {
         return Response.ok("Trallala").build();
+    }
+
+    @GET
+    @Path("/dockerstatus")
+    @Produces("text/plain")
+    public Response getDockerStatus() {
+        String response = "";
+
+        try {
+            Process p_docker = Runtime.getRuntime().exec("ps aux");
+            p_docker.waitFor();
+            InputStream respStream = p_docker.getInputStream();
+            if (p_docker.exitValue() != 0) {
+                respStream = p_docker.getErrorStream();
+            }
+            response += IOUtils.toString(respStream, "UTF-8");
+
+        } catch (IOException | InterruptedException e) {
+            return Response.ok(e.getMessage()).build();
+        }
+
+        return Response.ok(response).build();
     }
 
 }
