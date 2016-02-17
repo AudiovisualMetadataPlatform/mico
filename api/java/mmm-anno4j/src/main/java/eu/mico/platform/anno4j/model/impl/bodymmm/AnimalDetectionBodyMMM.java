d67c151bdde0de8b5980c37053613ddfd8a79581
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2016-02-17 16:39:03 +0100

    Added ObjectDetectionBody, AnimalDetectionBody inherits from it

--- api/java/mmm-anno4j/src/main/java/eu/mico/platform/anno4j/model/impl/bodymmm/AnimalDetectionBodyMMM.java
+++ api/java/mmm-anno4j/src/main/java/eu/mico/platform/anno4j/model/impl/bodymmm/AnimalDetectionBodyMMM.java
@@ -6,23 +6,6 @@ import eu.mico.platform.anno4j.model.namespaces.MMM;
 import org.openrdf.annotations.Iri;
 
 @Iri(MMM.ANIMAL_DETECTION_BODY)
-public interface AnimalDetectionBodyMMM extends BodyMMM {
+public interface AnimalDetectionBodyMMM extends ObjectDetectionBodyMMM {
 
-    @Iri(MMM.HAS_CONFIDENCE)
-    void setConfidence(Double confidence);
-
-    @Iri(MMM.HAS_CONFIDENCE)
-    String getConfidence();
-
-    @Iri(RDF.VALUE)
-    void setValue(String value);
-
-    @Iri(RDF.VALUE)
-    String getValue();
-
-    @Iri(MMM.HAS_EXTRACTION_VERSION)
-    void setExtractionVersion(String extractionVersion);
-
-    @Iri(MMM.HAS_EXTRACTION_VERSION)
-    String getExtractionVersion();
 }
