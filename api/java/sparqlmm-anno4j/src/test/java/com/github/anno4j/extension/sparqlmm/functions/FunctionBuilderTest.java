5aa029d52983a25fa54fb5a20fbffa5d2d1ad5f5
Thomas Kurz <thomas.kurz@salzburgresearch.at>
2016-04-12 14:44:16 +0200

    PUBLISHING-132 altered test to be multi platform compatible

--- api/java/sparqlmm-anno4j/src/test/java/com/github/anno4j/extension/sparqlmm/functions/FunctionBuilderTest.java
+++ api/java/sparqlmm-anno4j/src/test/java/com/github/anno4j/extension/sparqlmm/functions/FunctionBuilderTest.java
@@ -52,7 +52,7 @@ public class FunctionBuilderTest {
         Assert.assertTrue(interface_file.exists());
         Assert.assertFalse(interface_file.isDirectory());
 
-        Assert.assertEquals(4090, main_file.length());
+        Assert.assertTrue(main_file.length() >= 4090);
 
         File function_dir = Paths.get(folder.getRoot().getAbsolutePath(),"com/github/anno4j/extension/sparqlmm/functions").toFile();
 
