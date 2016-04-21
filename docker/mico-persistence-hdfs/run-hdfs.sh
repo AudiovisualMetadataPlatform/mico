a55c04d8b0992bfdb2960ebbbb63ca1e4ee04c28
Horst Stadler <horst.stadler@salzburgresearch.at>
2016-04-21 11:05:25 +0200

    FRAMEWORK-136: Improved HDFS server Dockerfile to allow easy configuration of HDFS hostname

--- docker/mico-persistence-hdfs/run-hdfs.sh
+++ docker/mico-persistence-hdfs/run-hdfs.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 function startup {
+	HDFS_HOSTNAME=localhost	
+	if [ "$HDFS_USE_HOSTNAME" == "true" ]; then
+		HDFS_HOSTNAME=$HOSTNAME
+	fi
+
+	echo -e "<!-- DO NOT CHANGE! File gets overwritten on startup. -->\n\n" | tee /etc/hadoop/core-site.xml /etc/hadoop/hdfs-site.xml >/dev/null
+	echo "#DO NOT CHANGE! File gets overwritten on startup" > /etc/hadoop/slaves
+	sed "s/%HDFS_HOSTNAME%/${HDFS_HOSTNAME}/g" /etc/hadoop/core-site.xml.tmpl >> /etc/hadoop/core-site.xml
+	sed "s/%HDFS_HOSTNAME%/${HDFS_HOSTNAME}/g" /etc/hadoop/hdfs-site.xml.tmpl >> /etc/hadoop/hdfs-site.xml
+	sed "s/%HDFS_HOSTNAME%/${HDFS_HOSTNAME}/g" /etc/hadoop/slaves.tmpl >> /etc/hadoop/slaves
+
 	service ssh start && \
 	echo exit |sudo -u hadoop ssh -o StrictHostKeyChecking=no $HOSTNAME && \
 	sudo -u hadoop /opt/hadoop/sbin/start-dfs.sh && \
-	((sleep 5 && echo -e "\n------------------------------------------------\nWait for image sync (safemode) to be finished...") &)
+	((sleep 5 && echo -e "\n------------------------------------------------\nHDFS is available via ${HDFS_HOSTNAME}\nWait for image sync (safemode) to be finished...") &)
 }
 
 
