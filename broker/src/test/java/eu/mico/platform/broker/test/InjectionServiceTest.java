aafcb16803abdd15bf54cc50b078a067a3461e4a
Luca Cuccovillo <luca.cuccovillo@idmt.fraunhofer.de>
2016-06-09 18:17:25 +0200

    [injection] added test for nasty items (aka both the item and at least one part have the correct semantic type)

--- broker/src/test/java/eu/mico/platform/broker/test/InjectionServiceTest.java
+++ broker/src/test/java/eu/mico/platform/broker/test/InjectionServiceTest.java
@@ -777,6 +777,197 @@ public class InjectionServiceTest extends BaseBrokerTest {
     	
     }
     
+    @SuppressWarnings("deprecation")
+	@Test
+    public void testNewInjectionWithNastyItem() throws IOException, InterruptedException, RepositoryException, URISyntaxException {
+    	
+    	Assume.assumeTrue(isRegistrationServiceAvailable);
+    	
+    	//setup extractors
+    	MockService abService = new MockServiceInjTest("A", "B");
+    	MockService abService1 =new MockServiceInjTest("A", "B");
+    	MockService abService2 =new MockServiceInjTest("A", "B");
+    	MockService acService = new MockServiceInjTest("A", "C");
+    	MockService bcService = new MockServiceInjTest("B", "C");
+    	
+    	
+    	//setup test routes
+    	
+    	String A_B_MICO_TEST =WorkflowServiceTest.createTestRoute(abService, "mico:A-B", "mico/test");
+    	String A_B_MICO_TEST1=WorkflowServiceTest.createTestRoute(abService1,"mico:A-B", "mico/test1");
+    	String A_B_MICO_TEST2=WorkflowServiceTest.createTestRoute(abService2,"mico:A-B", "mico/test2");    	
+    	String A_C_MICO_TEST =WorkflowServiceTest.createTestRoute(acService, "mico:A-C", "mico/test");
+    	String B_C_MICO_TEST =WorkflowServiceTest.createTestRoute(bcService, "mico:B-C", "mico/test");
+    	
+    	//and publish them
+    	
+    	Map<String,Integer> routeIds = new HashMap<String,Integer>();
+    	
+        routeIds.put(A_B_MICO_TEST, wManager.addWorkflow(USER, "A_B_MICO_TEST" ,A_B_MICO_TEST , "[]","[]"));
+    	routeIds.put(A_B_MICO_TEST1,wManager.addWorkflow(USER, "A_B_MICO_TEST1",A_B_MICO_TEST1, "[]","[]"));
+    	routeIds.put(A_B_MICO_TEST2,wManager.addWorkflow(USER, "A_B_MICO_TEST2",A_B_MICO_TEST2, "[]","[]"));
+    	routeIds.put(A_C_MICO_TEST, wManager.addWorkflow(USER, "A_C_MICO_TEST" ,A_C_MICO_TEST , "[]","[]"));
+    	routeIds.put(B_C_MICO_TEST, wManager.addWorkflow(USER, "B_C_MICO_TEST" ,B_C_MICO_TEST , "[]","[]"));
+
+    	Map<String,MockEndpoint> mocks = new HashMap<String,MockEndpoint>();    	
+
+    	//setup the mock endpoints
+    	mocks.put(A_B_MICO_TEST, getMockEndpoint("mock:auto-test-route-A-B-mico/test"));
+    	mocks.put(A_B_MICO_TEST1,getMockEndpoint("mock:auto-test-route-A-B-mico/test1"));
+    	mocks.put(A_B_MICO_TEST2,getMockEndpoint("mock:auto-test-route-A-B-mico/test2"));
+    	mocks.put(A_C_MICO_TEST, getMockEndpoint("mock:auto-test-route-A-C-mico/test"));
+    	mocks.put(B_C_MICO_TEST, getMockEndpoint("mock:auto-test-route-B-C-mico/test"));
+    	
+        
+        //try triggering all routes correctly, and verify that no mock is activated
+    	for(MockEndpoint m : mocks.values()){
+    		m.setExpectedCount(0);
+    	}
+    	
+    	//not-registered = broken = BAD_REQUEST
+    	
+    	Assert.assertEquals(Status.BAD_REQUEST.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test",routeIds.get(A_B_MICO_TEST)).getStatus());
+    	Assert.assertEquals(Status.BAD_REQUEST.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test1",routeIds.get(A_B_MICO_TEST1)).getStatus());
+    	Assert.assertEquals(Status.BAD_REQUEST.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test2",routeIds.get(A_B_MICO_TEST2)).getStatus());
+    	Assert.assertEquals(Status.BAD_REQUEST.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-C","mico/test", routeIds.get(A_C_MICO_TEST)).getStatus());
+    	Assert.assertEquals(Status.BAD_REQUEST.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:B-C","mico/test", routeIds.get(B_C_MICO_TEST)).getStatus());
+    	
+    	Thread.sleep(500);
+    	
+    	for(MockEndpoint m : mocks.values()){
+    		m.assertIsSatisfied();
+    	}
+    	
+    	//register the extractors, and repeat the same check
+    	registerExtractor(abService, "mico/test");
+    	registerExtractor(abService1, "mico/test1");
+    	registerExtractor(abService2, "mico/test2");
+    	registerExtractor(acService, "mico/test");
+    	registerExtractor(bcService, "mico/test");
+    	
+    	
+    	//registered but not connected = UNAVAILABLE
+    	
+    	Thread.sleep(500);
+    	
+    	Assert.assertEquals(Status.SERVICE_UNAVAILABLE.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test",routeIds.get(A_B_MICO_TEST)).getStatus());
+    	Assert.assertEquals(Status.SERVICE_UNAVAILABLE.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test1",routeIds.get(A_B_MICO_TEST1)).getStatus());
+    	Assert.assertEquals(Status.SERVICE_UNAVAILABLE.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test2",routeIds.get(A_B_MICO_TEST2)).getStatus());
+    	Assert.assertEquals(Status.SERVICE_UNAVAILABLE.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-C","mico/test", routeIds.get(A_C_MICO_TEST)).getStatus());
+    	Assert.assertEquals(Status.SERVICE_UNAVAILABLE.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:B-C","mico/test", routeIds.get(B_C_MICO_TEST)).getStatus());
+    	
+    	Thread.sleep(500);
+    	
+    	for(MockEndpoint m : mocks.values()){
+    		m.assertIsSatisfied();
+    	}
+    	
+    	
+    	//connect the extractors, and check that the mocks are activated only ONCE EACH
+
+    	connectExtractor(abService);
+    	connectExtractor(abService1);
+    	connectExtractor(abService2);
+    	connectExtractor(acService);
+    	connectExtractor(bcService);
+    	
+    	Thread.sleep(500);
+    	
+    	for(MockEndpoint m : mocks.values()){
+    		m.setExpectedCount(2);
+    	}
+    	
+    	Assert.assertEquals(Status.OK.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test",routeIds.get(A_B_MICO_TEST)).getStatus());
+    	Assert.assertEquals(Status.OK.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test1",routeIds.get(A_B_MICO_TEST1)).getStatus());
+    	Assert.assertEquals(Status.OK.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test2",routeIds.get(A_B_MICO_TEST2)).getStatus());
+    	Assert.assertEquals(Status.OK.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-C","mico/test", routeIds.get(A_C_MICO_TEST)).getStatus());
+    	Assert.assertEquals(Status.OK.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:B-C","mico/test", routeIds.get(B_C_MICO_TEST)).getStatus());
+    	
+    	Thread.sleep(500);
+    	
+    	for(MockEndpoint m : mocks.values()){
+    		m.assertIsSatisfied();
+    	}
+    	
+    	 //try triggering all routes INCORRECTLY, and verify that no mock is activated
+    	for(MockEndpoint m : mocks.values()){
+    		m.reset();
+    		m.setExpectedCount(0);
+    	}
+    	
+    	//requests with correct syntactic type, but wrong mime type
+    	Assert.assertEquals(Status.BAD_REQUEST.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test1",routeIds.get(A_B_MICO_TEST)).getStatus());
+    	Assert.assertEquals(Status.BAD_REQUEST.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test2",routeIds.get(A_B_MICO_TEST1)).getStatus());
+    	Assert.assertEquals(Status.BAD_REQUEST.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test",routeIds.get(A_B_MICO_TEST2)).getStatus());
+    	
+    	//request with correct mime type, but wrong syntactic type
+    	Assert.assertEquals(Status.BAD_REQUEST.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-B","mico/test", routeIds.get(A_C_MICO_TEST)).getStatus());
+    	
+    	//request where both types are wrong
+    	Assert.assertEquals(Status.BAD_REQUEST.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:A-C","mico/test2s", routeIds.get(B_C_MICO_TEST)).getStatus());
+    	
+    	Thread.sleep(500);
+    	
+    	for(MockEndpoint m : mocks.values()){
+    		m.assertIsSatisfied();
+    	}
+
+    	
+    	//try triggering one routes correctly, but with a FAILING EXTRACTOR, and verify that the mock is not activated
+
+    	MockService abFailingService = new MockFailingServiceInjTest("ERROR", "B");
+    	registerExtractor(abFailingService, "mico/error");
+    	connectExtractor(abFailingService);
+    	
+    	String ERROR_B_MICO_FAILING_TEST =WorkflowServiceTest.createTestRoute(abFailingService, "mico:ERROR-B", "mico/error");
+    	routeIds.put(ERROR_B_MICO_FAILING_TEST, wManager.addWorkflow(USER, "ERROR_B_MICO_FAILING_TEST" ,ERROR_B_MICO_FAILING_TEST , "[]","[]"));
+    	
+    	mocks.put(ERROR_B_MICO_FAILING_TEST, getMockEndpoint("mock:auto-test-route-ERROR-B-mico/error"));
+    	mocks.get(ERROR_B_MICO_FAILING_TEST).setExpectedCount(2);
+    	
+
+    	Assert.assertEquals(Status.OK.getStatusCode(),
+    			triggerRouteWithNastyItem("mico:ERROR-B","mico/error",routeIds.get(ERROR_B_MICO_FAILING_TEST)).getStatus());
+
+    	mocks.get(ERROR_B_MICO_FAILING_TEST).assertIsSatisfied();
+    	//TODO: find a way to assert that the received body holds an error state
+    	
+    	//cleanup
+    	
+    	unregisterExtractor(abService);
+    	unregisterExtractor(abService1);
+    	unregisterExtractor(abService2);
+    	unregisterExtractor(acService);
+    	unregisterExtractor(bcService);
+    	unregisterExtractor(abFailingService);
+    	
+    	for(Integer id : routeIds.values()){
+    		wManager.deleteWorkflow(id);
+    	}
+    	assert(wManager.listWorkflows(USER).isEmpty());
+    	
+    }
+    
     private Item broadcastSimpleItem(String mimeType) throws RepositoryException, IOException, InterruptedException
     {
     	 PersistenceService ps = broker.getPersistenceService();
@@ -933,6 +1124,54 @@ public class InjectionServiceTest extends BaseBrokerTest {
             	 "syntacticalType", equalTo(expectedNewType))));
             	 
             	 Assert.assertEquals(2, parts.size());
+             }
+             
+             return r;
+         }
+         catch(Exception e){
+        	 log.error("Unexpected exception: ");
+        	 e.printStackTrace();
+        	 Assert.fail();
+        	 return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+         }
+         finally{
+        	 if(item != null){
+                 ps.deleteItem(item.getURI());
+             }
+        	 
+        }
+         
+         
+    }
+    
+    private Response triggerRouteWithNastyItem(String syntacticType,String mimeType,Integer routeId) throws RepositoryException, IOException, InterruptedException
+    {
+    	 PersistenceService ps = broker.getPersistenceService();
+         Item item = null;
+         try {
+             item = ps.createItem();
+             item.setSemanticType(syntacticType+" over "+mimeType);
+             item.setSyntacticalType(syntacticType);
+             item.getAsset().setFormat(mimeType);
+             
+             Part part =item.createPart(new URIImpl("urn:org.example.injection"));
+             part.setSemanticType(syntacticType+" over "+mimeType);
+             part.setSyntacticalType(syntacticType);
+             part.getAsset().setFormat(mimeType);
+
+             Response r = injService.submitItem(item.getURI().stringValue(), routeId);
+             
+             Thread.sleep(500);
+             
+             if(r.getStatus()==Status.OK.getStatusCode() && !mimeType.contentEquals("mico/error")){
+            	 
+            	 String expectedNewType = String.valueOf(syntacticType.charAt(syntacticType.length()-1));
+            	 
+            	 Set<Part> parts = ImmutableSet.copyOf(item.getParts());
+            	 Assert.assertThat(parts, Matchers.<Part>hasItem(hasProperty(
+            	 "syntacticalType", equalTo(expectedNewType))));
+            	 
+            	 Assert.assertEquals(3, parts.size());
              }
              
              return r;
