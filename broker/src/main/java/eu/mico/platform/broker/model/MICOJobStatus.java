57c96e599d5905ca49eb61c0d6c7deadc75cd144
Luca Cuccovillo <luca.cuccovillo@idmt.fraunhofer.de>
2016-06-13 11:36:17 +0200

    [broker] JobStatus: added creation date, improved error description handling

--- broker/src/main/java/eu/mico/platform/broker/model/MICOJobStatus.java
+++ broker/src/main/java/eu/mico/platform/broker/model/MICOJobStatus.java
@@ -1,35 +1,45 @@
 package eu.mico.platform.broker.model;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 //holds a 
 public class MICOJobStatus implements Runnable{
 
 	private static Logger log = LoggerFactory.getLogger(MICOJobStatus.class);
 	private List<CamelJob> activeRoutes = new ArrayList<CamelJob>();	
+	private Date created  = new Date();
 	
 	private boolean isFinished = false;
 	private boolean hasError  = false;
+	private String error = "";
 
 	@Override
 	public void run() {
 		//start all the required routes
 		for (CamelJob r : activeRoutes){
 			r.run();
-			hasError=hasError || r.hasError();
 		}
 		
 		//wait for all of them to finish
 		for (CamelJob r : activeRoutes){
 			try {
 				r.join();
+				hasError=hasError || r.hasError();
+				
+				if(r.hasError()){
+					error = error + r.getErrorMessage() + "\n";
+				}
+				
 			} catch (InterruptedException e) {
 				hasError=true;
 				isFinished=true;
+				error = "Execution interrupted abruptly";
 				break;
 			}
 		}
@@ -55,6 +65,14 @@ public class MICOJobStatus implements Runnable{
 	private void handleCompletion(){
 		isFinished=true;
 		log.info("A mico job terminated");
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public String getErrorMessage() {
+		return error;
 	}
 
 	
