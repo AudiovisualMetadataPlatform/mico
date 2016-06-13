d96a598e606d4313a857cd42ad9e11f3492adda8
Luca Cuccovillo <luca.cuccovillo@idmt.fraunhofer.de>
2016-06-13 11:39:00 +0200

    [broker] added classes for wrapping a camel state

---
+++ broker/src/main/java/eu/mico/platform/broker/model/BrokerV3ItemState.java
@@ -0,0 +1,57 @@
+package eu.mico.platform.broker.model;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.openrdf.model.URI;
+
+import eu.mico.platform.broker.api.ItemState;
+
+public class BrokerV3ItemState implements ItemState{
+	
+	MICOJobStatus s;
+	
+	public BrokerV3ItemState(MICOJobStatus s){
+		this.s = s;
+	}
+
+	public boolean hasError() {
+		return s.hasError();
+	}
+	
+
+	public Date getCreated() {
+		return s.getCreated();
+	}
+
+	@Override
+	public boolean isFinalState() {
+		return s.isFinished();
+	}
+
+	@Override
+	@Deprecated
+	public Set<Transition> getPossibleTransitions() {
+		return new HashSet<Transition>();
+	}
+
+	@Override
+	@Deprecated
+	public Map<URI, TypeDescriptor> getStates() {
+		return new HashMap<URI,TypeDescriptor>();
+	}
+
+	@Override
+	@Deprecated
+	public Map<String, Transition> getProgress() {
+		return new HashMap<String,Transition>();
+	}
+
+	@Override
+	public String getError() {
+		return s.getErrorMessage();
+	}
+}
