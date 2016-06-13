c4e3fcff358ad8d91d74685f49e492f441d848e3
Luca Cuccovillo <luca.cuccovillo@idmt.fraunhofer.de>
2016-06-13 16:14:37 +0200

    [rabbitmp comp] changed default vaule of host to mico-platform

--- rabbitmq-comp/src/main/java/eu/mico/platform/camel/MicoRabbitEndpoint.java
+++ rabbitmq-comp/src/main/java/eu/mico/platform/camel/MicoRabbitEndpoint.java
@@ -33,8 +33,8 @@ public class MicoRabbitEndpoint extends DefaultEndpoint {
     @UriParam(defaultValue = "10")
     private int option = 10;
 
-    @UriParam(name="host", defaultValue = "mico-box")
-    private String host = "mico-box";
+    @UriParam(name="host", defaultValue = "mico-platform")
+    private String host = "mico-platform";
     @UriParam(name="vhost", defaultValue = "/")
     private String virtualHost = "/";
     
